8762e53d752649e60cf63bc7f511601a811397ed
Karol Obrębski <karolbreb@gmail.com>
2023-09-11 22:16:23 +0200

    test4

--- validator.js
+++ validator.js
@@ -6,20 +6,23 @@ function isValidToken(token) {
     return validTokens.includes(token);
 }
 
-// Handle a request with a token
-function handleRequest(token) {
-    if (isValidToken(token)) {
+// Handle a request with a token sent from Python
+function handleRequest() {
+    // Extract the token from the query parameters of the URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const tokenValue = urlParams.get('token');
+
+    if (isValidToken(tokenValue)) {
         // Grant permission to execute Python code
-        console.log('Permission granted for token:', token);
+        console.log('Permission granted for token:', tokenValue);
         // Perform any necessary actions here
         // Return a response or execute Python code
     } else {
         // Deny permission
-        console.log('Permission denied for token:', token);
+        console.log('Permission denied for token:', tokenValue);
         // Return an error response or deny Python code execution
     }
 }
 
-// Example usage: replace 'tokenValue' with the actual token from the Python code
-const tokenValue = '0001'; // Replace with the token from the Python computer
-handleRequest(tokenValue);
+// Call the handleRequest function when the page loads
+window.addEventListener('load', handleRequest);
