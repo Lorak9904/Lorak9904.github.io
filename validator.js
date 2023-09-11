d4810994f4f35c7d33a6b187443b58f9dba6c7e1
Karol Obrębski <karolbreb@gmail.com>
2023-09-11 22:28:18 +0200

    test6

--- validator.js
+++ validator.js
@@ -4,7 +4,7 @@ const validTokens = ['0001', '0002', '0003'];
 // Function to check if a token is valid
 function isValidToken(token) {
     return validTokens.includes(token);
-} 
+}
 
 // Handle a request with a token sent from Python
 function handleRequest() {
@@ -12,16 +12,25 @@ function handleRequest() {
     const urlParams = new URLSearchParams(window.location.search);
     const tokenValue = urlParams.get('token');
 
+    let responseMessage = '';
+
     if (isValidToken(tokenValue)) {
         // Grant permission to execute Python code
+        responseMessage = 'Permission granted';
         console.log('Permission granted for token:', tokenValue);
         // Perform any necessary actions here
-        // Return a response or execute Python code
+        // You can also return additional data in the responseMessage
     } else {
         // Deny permission
+        responseMessage = 'Permission denied';
         console.log('Permission denied for token:', tokenValue);
-        // Return an error response or deny Python code execution
+        // You can provide an error message or additional information here
     }
+
+    // Send the response message back to Python
+    // In this example, we're using alert, but you can replace this with any method
+    // to communicate the response back to your Python script
+    alert(responseMessage);
 }
 
 // Call the handleRequest function when the page loads
